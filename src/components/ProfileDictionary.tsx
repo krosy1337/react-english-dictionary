014bece8370222e45994ce26997a137edbf30f0e
Alexey Egorov <krosy1337@gmail.com>
2022-04-23 09:47:24 +0300

    Fix dictionary in profile page

--- src/components/ProfileDictionary.tsx
+++ src/components/ProfileDictionary.tsx
@@ -1,6 +1,6 @@
-import React, {FC} from 'react'
+import React, {FC, useState} from 'react'
 import {DataGrid, GridColDef, GridSelectionModel} from "@mui/x-data-grid"
-import {Box, Typography} from "@mui/material"
+import {Box, Popover, Typography} from "@mui/material"
 import {useAppSelector} from "../hooks/redux"
 
 interface IProfileTranslate {
@@ -14,6 +14,8 @@ interface ProfileDictionaryProps {
 }
 
 const ProfileDictionary: FC<ProfileDictionaryProps> = ({setSelectedRows}) => {
+    const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null)
+    const [popoverValue, setPopoverValue] = useState<string>('')
     const {dictionary} = useAppSelector(state => state.user)
     const columns: GridColDef[] = [
         {
@@ -36,6 +38,22 @@ const ProfileDictionary: FC<ProfileDictionaryProps> = ({setSelectedRows}) => {
             translate: t.words[1].text[0]
         }
     })
+
+    const onPopoverClose = () => {
+        setAnchorEl(null)
+    }
+    const onPopoverOpen = (e: React.MouseEvent<HTMLElement>) => {
+        const field = e.currentTarget.dataset.field!
+        const id = e.currentTarget.parentElement!.dataset.id!
+        const row: any = profileDictionary?.find((r) => r.id === id)!
+        const cellContent = e.currentTarget.children[0]
+        const [paddingLeft, paddingRight] = [parseInt(getComputedStyle(e.currentTarget).paddingLeft),
+            parseInt(getComputedStyle(e.currentTarget).paddingRight)]
+        if (e.currentTarget.clientWidth - paddingLeft - paddingRight < cellContent?.scrollWidth) {
+            setPopoverValue(row[field])
+            setAnchorEl(e.currentTarget)
+        }
+    }
     const onRowsSelect = (newSelectionModel: GridSelectionModel) => {
         if (newSelectionModel.length) {
             setSelectedRows(newSelectionModel)
@@ -43,16 +61,38 @@ const ProfileDictionary: FC<ProfileDictionaryProps> = ({setSelectedRows}) => {
         }
         setSelectedRows(null)
     }
+
     return (
         <Box sx={{
             height: 400,
             width: "100%",
         }}>
+            <Popover sx={{pointerEvents: "none"}} open={!!anchorEl} anchorEl={anchorEl}
+                     anchorOrigin={{
+                         vertical: "bottom",
+                         horizontal: "left",
+                     }}
+                     transformOrigin={{
+                         vertical: "top",
+                         horizontal: "left",
+                     }}
+                     onClose={onPopoverClose}
+                     disableRestoreFocus
+            >
+                <Typography sx={{padding: 1,}}>{popoverValue}</Typography>
+            </Popover>
             {
                 profileDictionary ?
                     <DataGrid rows={profileDictionary} columns={columns} pageSize={5}
-                              rowsPerPageOptions={[5]} componentsProps={{}} checkboxSelection
-                              disableSelectionOnClick onSelectionModelChange={onRowsSelect}/>
+                              rowsPerPageOptions={[5]} checkboxSelection
+                              disableSelectionOnClick onSelectionModelChange={onRowsSelect}
+                              getRowClassName={(params) => `app-theme--${params.row.isCorrect}`}
+                              componentsProps={{
+                                  cell: {
+                                      onMouseEnter: onPopoverOpen,
+                                      onMouseLeave: onPopoverClose,
+                                  },
+                              }}/>
                     :
                     <Typography variant="h1" fontSize="3rem" align="center">Ваш словарь пуст</Typography>
             }
